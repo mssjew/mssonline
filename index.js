80261435ce93685eba1e6cdc04a219f28cd50590
mssjew <86316072+mssjew@users.noreply.github.com>
2021-10-01 18:34:47 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-console.log("does this work?");
+const str = "dddd";
+
+const p = document.getElementById("tester");
+
 
 
 axios.get("https://sheets.googleapis.com/v4/spreadsheets/1OJaJ-yJX6vDt6PtUcw4KK5T59JKYAAd4j0NkZext6Jo/values/Sep21!A1:B2?key=AIzaSyDmbXdZsgesHy5afOQOZSr9hgDeQNTC6Q4")
 .then(resp => {
   console.log(resp);
+  p.textContent = resp.data.majorDimension;
+  
+
 })
 .catch(err => {
   console.error(err);
