36d25498a6a7db7958c705a9f66b3ffb2e3ab95a
mssjew <86316072+mssjew@users.noreply.github.com>
2021-10-02 20:13:14 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -179,27 +179,114 @@ axios
 // -------------------- BUY SIDE END ---------------------
 
 
+
+// -------------------- Live Price --------------------
 const priceSpan = document.getElementById("price");
 
+var myHeaders = new Headers();
+myHeaders.append("x-access-token", "goldapi-f20pyjatkuagctl5-io");
+myHeaders.append("Content-Type", "application/json");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
 
 
-// var myHeaders = new Headers();
-// myHeaders.append("x-access-token", "goldapi-f20pyjatkuagctl5-io");
-// myHeaders.append("Content-Type", "application/json");
-
-// var requestOptions = {
-//   method: 'GET',
-//   headers: myHeaders,
-//   redirect: 'follow'
-// };
-
-
-// fetch("https://www.goldapi.io/api/XAU/USD", requestOptions)
-//   .then(response => response.json())
-//   .then(result => priceSpan.textContent = result.price)
-//   .catch(error => console.log('error', error));
-
+fetch("https://www.goldapi.io/api/XAU/USD", requestOptions)
+  .then(response => response.json())
+  .then(result => priceSpan.textContent = result.price)
+  .catch(error => console.log('error', error));
 
 
 
 
+
+
+// -------------------- Closing Today --------------------
+
+function cardMaker(e) {
+
+  function getNum(arr) {
+    const profit = arr.split("");
+    profit.splice(0, 4);
+  
+    for (let i = 0; i < profit.length; i++){
+        if (profit[i] === ",") {
+          profit.splice(i,1);
+        }
+    }
+  
+    let num = profit.join('');
+  
+    let retval = parseFloat(num);
+
+    return retval
+  }
+
+
+  const totalProfit = getNum(e[3]);
+  const perTT = getNum(e[4]);
+
+  const cardDiv = document.createElement("div");
+  const divr1 = document.createElement("div");
+  const divr2 = document.createElement("div");
+  const divr3 = document.createElement("div");
+  const divr4 = document.createElement("div");
+  const divr5 = document.createElement("div");
+  const divSep = document.createElement("br");
+ 
+  
+  cardDiv.appendChild(divr1);
+  cardDiv.appendChild(divr2);
+  cardDiv.appendChild(divr3);
+  cardDiv.appendChild(divr4);
+  cardDiv.appendChild(divr5);
+  cardDiv.appendChild(divSep);
+
+
+  cardDiv.classList.add("card");
+  divr1.classList.add("row1");
+  divr2.classList.add("row2");
+  divr3.classList.add("row3");
+  divr4.classList.add("row4");
+  divr5.classList.add("row5");
+
+
+  divr1.textContent = "Amount: " + e[0];
+  divr2.textContent = "Buy Price: " + e[1];
+  divr3.textContent = "Sell Price: " + e[2];
+  divr4.textContent = "Profit: BD " + totalProfit;
+  divr5.textContent = "Per TT: BD " + perTT;
+
+  if (totalProfit && perTT > 0) {
+    divr4.classList.add("rowProfit");
+    divr5.classList.add("rowProfit");
+  } else {
+    divr4.classList.add("rowLoss")
+    divr5.classList.add("rowProfit");
+  }
+  
+
+  return cardDiv;
+}
+
+
+const todaySection = document.getElementById("todaySec");
+
+const amtToday = "Summary!B49:F60";
+
+// Today Closing
+axios
+  .get(
+    `https://sheets.googleapis.com/v4/spreadsheets/1OJaJ-yJX6vDt6PtUcw4KK5T59JKYAAd4j0NkZext6Jo/values/${amtToday}?key=AIzaSyDmbXdZsgesHy5afOQOZSr9hgDeQNTC6Q4`
+  )
+  .then((resp) => {
+    resp.data.values.forEach(function(e) {
+      todaySection.appendChild(cardMaker(e));
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
